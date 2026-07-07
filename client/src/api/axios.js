77588346a19e607ed37235197f8b3fccf14caa50
Tanmay Kumar <tanmaykumar7106@gmail.com>
2026-07-07 18:56:22 +0530

    feat: prepare frontend for production

--- client/src/api/axios.js
+++ client/src/api/axios.js
@@ -1,20 +1,21 @@
 import axios from "axios";
 
 const api = axios.create({
-baseURL: "http://localhost:5000/api",
-headers: {
-    "Content-Type": "application/json",
-},
+    baseURL:
+        import.meta.env.VITE_API_URL || "http://localhost:5000/api",
+    headers: {
+        "Content-Type": "application/json",
+    },
 });
 
 api.interceptors.request.use((config) => {
-const token = localStorage.getItem("token");
+    const token = localStorage.getItem("token");
 
-if (token) {
-    config.headers.Authorization = `Bearer ${token}`;
-}
+    if (token) {
+        config.headers.Authorization = `Bearer ${token}`;
+    }
 
-return config;
+    return config;
 });
 
 export default api;
